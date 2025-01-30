55e4c0ec387f844b8189427172f39129bbfe24ce
Daniel Herrmann <daniel.herrmann@makerspace-darmstadt.de>
2025-01-30 21:09:45 +0100

    fix: export DESFireKey via init

--- src/desfire/__init__.py
+++ src/desfire/__init__.py
@@ -1,5 +1,6 @@
 from .DESFire import DESFire
 from .diversify import diversify_key
+from .key.key import DESFireKey
 from .pcsc import PCSCDevice
 
-__all__ = ["DESFire", "PCSCDevice", "diversify_key"]
+__all__ = ["DESFire", "PCSCDevice", "diversify_key", "DESFireKey"]
